2f5aad4a3bd6005bd98f3e826bba32fd1e6ac540
刘利民 <liulimin@dushu365.com>
2023-03-15 16:40:59 +0800

    feature:购物车Bar、UI

--- good-shop/src/views/Cart/index.tsx
+++ good-shop/src/views/Cart/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import { Bar, Empty, Full } from './components';
+
+const data = [];
 
 const Cart = () => {
-  return <Box>Cart</Box>;
+  return (
+    <Box>
+      <Bar />
+      <Box w='100%' h='100vh' bgColor={'gray.100'}>
+        {data.length > 0 ? <Full /> : <Empty />}
+      </Box>
+    </Box>
+  );
 };
 
 export default Cart;
